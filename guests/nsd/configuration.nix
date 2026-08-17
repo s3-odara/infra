0558a7f9274b109b2b411a97c43d2ee04ed39997
s3-odara <haruta@s3-odara.net>
2026-08-18 00:10:53 +0900

    update dns

--- guests/nsd/configuration.nix
+++ guests/nsd/configuration.nix
@@ -37,43 +37,25 @@
         odarah.org. 3600 IN SOA ns.odarah.org. HOSTMASTER.S3-ODARA.NET. 2026081601 3600 1800 604800 600
         odarah.org. 3600 IN NS ns.odarah.org.
         odarah.org. 3600 IN A 127.0.0.1
-        ns.odarah.org. 3600 IN A 133.117.77.64
-        xmpp.odarah.org. 3600 IN A 133.117.77.64
+        ns.odarah.org. 120 IN A 133.117.77.64
+        xmpp.odarah.org. 120 IN A 133.117.77.64
         _xmpp-client._tcp.xmpp.odarah.org. 3600 IN SRV 0 5 5222 xmpp.odarah.org.
         _xmpps-client._tcp.xmpp.odarah.org. 3600 IN SRV 0 5 5223 xmpp.odarah.org.
         _xmpp-server._tcp.xmpp.odarah.org. 3600 IN SRV 0 5 5269 xmpp.odarah.org.
-        conference.xmpp.odarah.org. 3600 IN A 133.117.77.64
-        share.xmpp.odarah.org. 3600 IN A 133.117.77.64
-        www.odarah.org. 3600 IN A 35.185.44.232
-        www.odarah.org. 3600 IN AAAA 2600:1901:0:7b8a::
+        conference.xmpp.odarah.org. 120 IN A 133.117.77.64
+        share.xmpp.odarah.org. 120 IN A 133.117.77.64
         odarah.org. 3600 IN CAA 0 issuemail ";"
         odarah.org. 3600 IN CAA 0 issuewild ";"
         odarah.org. 3600 IN CAA 0 issue "letsencrypt.org"
         odarah.org. 3600 IN CAA 0 iodef "mailto:hostmaster@s3-odara.net"
-        mbo0003._domainkey.mailinglist.odarah.org. 3600 IN CNAME mbo0003._domainkey.odarah.org.
-        mbo0004._domainkey.mailinglist.odarah.org. 3600 IN CNAME mbo0004._domainkey.odarah.org.
         odarah.org. 3600 IN LOC 35 54 33.931 N 138 36 44.221 E 2418.43m 0.00m 0.00m 0.00m
-        mailinglist.odarah.org. 3600 IN MX 20 mxext3.mailbox.org.
-        mailinglist.odarah.org. 3600 IN MX 10 mxext2.mailbox.org.
-        mailinglist.odarah.org. 3600 IN MX 10 mxext1.mailbox.org.
-        odarah.org. 86400 IN MX 20 mxext3.mailbox.org.
-        odarah.org. 86400 IN MX 10 mxext2.mailbox.org.
-        odarah.org. 86400 IN MX 10 mxext1.mailbox.org.
         _dmarc.mailinglist.odarah.org. 3600 IN TXT "v=DMARC1; p=none; sp=reject; aspf=s; adkim=s; rua=mailto:mailreport@s3-odara.net; ruf=mailto:postmaster@s3-odara.net; fo=1"
         _dmarc.odarah.org. 3600 IN TXT "v=DMARC1; p=reject; sp=reject; aspf=s; adkim=s; rua=mailto:mailreport@s3-odara.net; ruf=mailto:postmaster@s3-odara.net; fo=1"
-        mailinglist.odarah.org. 3600 IN TXT "v=spf1 include:mailbox.org ~all"
+        mailinglist.odarah.org. 3600 IN TXT "v=spf1 ~all"
         mailinglist.odarah.org._report._dmarc.odarah.org. 3600 IN TXT "v=DMARC1;"
-        mbo0001._domainkey.mailinglist.odarah.org. 3600 IN TXT "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2K4PavXoNY8eGK2u61LIQlOHS8f5sWsCK5b+HMOfo0M+aNHwfqlVdzi/IwmYnuDKuXYuCllrgnxZ4fG4yVaux58v9grVsFHdzdjPlAQfp5rkiETYpCMZwgsmdseJ4CoZaosPHLjPum" "FE/Ua2WAQQljnunsM9TONM9L6KxrO9t5IISD1XtJb0bq1lVI/e72k3mnPd/q77qzhTDmwN4TSNJZN8sxzUJx9HNSMRRoEIHSDLTIJUK+Up8IeCx0B7CiOzG5w/cHyZ3AM5V8lkqBaTDK46AwTkTVGJf59QxUZArG3FEH5vy9HzDmy0tGG+053/x4RqkhqMg5/ClDm+lp" "ZqWwIDAQAB"
-        mbo0001._domainkey.odarah.org. 3600 IN TXT "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2K4PavXoNY8eGK2u61LIQlOHS8f5sWsCK5b+HMOfo0M+aNHwfqlVdzi/IwmYnuDKuXYuCllrgnxZ4fG4yVaux58v9grVsFHdzdjPlAQfp5rkiETYpCMZwgsmdseJ4CoZaosPHLjPum" "FE/Ua2WAQQljnunsM9TONM9L6KxrO9t5IISD1XtJb0bq1lVI/e72k3mnPd/q77qzhTDmwN4TSNJZN8sxzUJx9HNSMRRoEIHSDLTIJUK+Up8IeCx0B7CiOzG5w/cHyZ3AM5V8lkqBaTDK46AwTkTVGJf59QxUZArG3FEH5vy9HzDmy0tGG+053/x4RqkhqMg5/ClDm+lp" "ZqWwIDAQAB"
-        mbo0002._domainkey.mailinglist.odarah.org. 3600 IN TXT "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqxEKIg2c48ecfmy/+rj35sBOhdfIYGNDCMeHy0b36DX6MNtS7zA/VDR2q5ubtHzraL5uUGas8kb/33wtrWFYxierLRXy12qj8ItdYCRugu9tXTByEED05WdBtRzJmrb8YBMfeK0E0K" "3wwoWfhIk/wzKbjMkbqYBOTYLlIcVGQWzOfN7/n3n+VChfu6sGFK3k2qrJNnw22iFy4C8Ks7j77+tCpm0PoUwA2hOdLrRw3ldx2E9PH0GVwIMJRgekY6cS7DrbHrj/AeGlwfwwCSi9T23mYvc79nVrh2+82ZqmkpZSTD2qq+ukOkyjdRuUPck6e2b+x141Nzd81dIZVf" "OEiwIDAQAB"
-        mbo0002._domainkey.odarah.org. 3600 IN TXT "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqxEKIg2c48ecfmy/+rj35sBOhdfIYGNDCMeHy0b36DX6MNtS7zA/VDR2q5ubtHzraL5uUGas8kb/33wtrWFYxierLRXy12qj8ItdYCRugu9tXTByEED05WdBtRzJmrb8YBMfeK0E0K" "3wwoWfhIk/wzKbjMkbqYBOTYLlIcVGQWzOfN7/n3n+VChfu6sGFK3k2qrJNnw22iFy4C8Ks7j77+tCpm0PoUwA2hOdLrRw3ldx2E9PH0GVwIMJRgekY6cS7DrbHrj/AeGlwfwwCSi9T23mYvc79nVrh2+82ZqmkpZSTD2qq+ukOkyjdRuUPck6e2b+x141Nzd81dIZVf" "OEiwIDAQAB"
-        mbo0003._domainkey.odarah.org. 3600 IN TXT "v=DKIM1; k=ed25519; p=k1A30Je8LhsVpcOQrwHOnw0OUj3BP0gXh8nHOgl6t4Y="
-        mbo0004._domainkey.odarah.org. 3600 IN TXT "v=DKIM1; k=ed25519; p=mLyBxtALVny3i4fn1fgHiXWWzk4XtNbmq4Jcmfkd8p8="
         _mta-sts.odarah.org. 3600 IN TXT "v=STSv1; id=20240407003138"
         odarah.org. 3600 IN TXT "my pgp fpr: B353 1E57 3A66 76E8 A920 60B3 879D 4D00 108D 4015"
-        odarah.org. 3600 IN TXT "v=spf1 include:mailbox.org ~all"
-        _gitlab-pages-verification-code.www.odarah.org. 3600 IN TXT "gitlab-pages-verification-code=af367ae4323acd54e4933f0da74773dc"
-        _acme-challenge.conference.xmpp.odarah.org. 30 IN TXT "Uhra1SiXxxCma-XMdCJRlCVtRilEBvPPcRdGsv1ky8k"
+        odarah.org. 3600 IN TXT "v=spf1 ~all"
       '';
 
       "51.72.179.167.in-addr.arpa.".data = ''
